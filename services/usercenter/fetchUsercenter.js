bad56e47c2ea0105081d91b8a8a756a77a3ac1f0
test <test666>
2022-07-02 23:48:59 +0800

    feat：用户授权逻辑开发

--- services/usercenter/fetchUsercenter.js
+++ services/usercenter/fetchUsercenter.js
@@ -1,17 +1,46 @@
-import { config } from '../../config/index';
+import {
+  config
+} from '../../config/index';
 
 /** 获取个人中心信息 */
 function mockFetchUserCenter() {
-  const { delay } = require('../_utils/delay');
-  const { genUsercenter } = require('../../model/usercenter');
+  const {
+    delay
+  } = require('../_utils/delay');
+  const {
+    genUsercenter
+  } = require('../../model/usercenter');
   return delay(200).then(() => genUsercenter());
 }
 
+
 /** 获取个人中心信息 */
 export function fetchUserCenter() {
+  wx.showLoading({
+    title: '刷新中',
+  })
   if (config.useMock) {
-    return mockFetchUserCenter();
+    var result = mockFetchUserCenter()
+
+    wx.getUserProfile({
+      desc: '用于完善会员资料',
+      success: (res) => {
+        console.log(res.userInfo);
+        wx.showToast({
+          title: res.userInfo.nickName,
+        })
+      }
+    })
+
+    wx.showToast({
+      title: 'toast',
+    })
+
+    return result;
+
   }
+
+
   return new Promise((resolve) => {
     resolve('real api');
   });
