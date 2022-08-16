b3dfe3f2af7020c399cd2502ce413aa65b7f0d7a
CFNet <>
2022-08-16 23:09:55 +0800

    update 分类添加请求

--- services/good/fetchCategoryList.js
+++ services/good/fetchCategoryList.js
@@ -1,18 +1,39 @@
-import { config } from '../../config/index';
+import {
+  config
+} from '../../config/index';
 
 /** 获取商品列表 */
 function mockFetchGoodCategory() {
-  const { delay } = require('../_utils/delay');
-  const { getCategoryList } = require('../../model/category');
+  const {
+    delay
+  } = require('../_utils/delay');
+  const {
+    getCategoryList
+  } = require('../../model/category');
   return delay().then(() => getCategoryList());
 }
 
 /** 获取商品列表 */
 export function getCategoryList() {
-  if (config.useMock) {
-    return mockFetchGoodCategory();
-  }
+  // if (config.useMock) {
+  // return mockFetchGoodCategory();
+  // }
   return new Promise((resolve) => {
-    resolve('real api');
+    wx.request({
+      url: 'http://8.136.244.224/web/category/tree',
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': wx.getStorageSync('userToken')
+      },
+      success: function (res) {
+        if (res) {
+          resolve(res.data.data);
+        }
+      },
+      fail: function (err) {
+        console.log('rjl');
+      }
+    })
   });
 }
